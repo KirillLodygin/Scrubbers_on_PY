24c33b6509b10d10f487631e313988a4be2ccd84
Kirilll <vilyam13@gmail.com>
2021-07-16 18:02:27 +0300

    requests + BeautifulSoup + try/except

---
+++ chapter1/3-beautifulSoup.py
@@ -0,0 +1,16 @@
+#! python3
+# -*- coding: utf8 -*-
+
+import requests, bs4
+
+res = requests.get('http://www.pythonscraping.com/pages/page1.html')
+try:
+	res.raise_for_status()
+except Exception as exc:
+	print('Возникла проблема: %s' % exc)
+
+if res is None:
+	print("URL is not found")
+else:
+	bsObj = bs4.BeautifulSoup(res.text)
+	print(bsObj.h1)
---
+++ chapter1/4-beautifuiSoup.py
@@ -0,0 +1,30 @@
+#! python3
+# -*- coding: utf8 -*-
+
+import requests, bs4
+
+
+def getTitle(url):
+	res = requests.get(url)
+	
+	try:
+		res.raise_for_status()
+	except Exception as exc:
+		print('Возникла проблема: %s' % exc)
+		return None
+	
+	try:
+		bsObj = bs4.BeautifulSoup(res.text)
+		title = bsObj.body.h1
+	except Exception as exc:
+		return None
+	
+	return title
+
+
+title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+
+if title is None:
+	print('Title could not be found')
+else:
+	print(title)
